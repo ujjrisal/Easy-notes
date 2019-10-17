a855f9d8c10039c8225bd72354a4e250867216c5
Ujjwal <ujjrisal@gmail.com>
2019-10-17 15:28:52 +1100

    3 new files added

---
+++ src/main/java/com/example/easynotes/service/BusinessService.java
@@ -0,0 +1,4 @@
+package com.example.easynotes.service;
+
+public class BusinessService {
+}
---
+++ src/main/java/com/example/easynotes/service/RoleService.java
@@ -0,0 +1,5 @@
+package com.example.easynotes.service;
+
+public class RoleService {
+    // This is a new class
+}
